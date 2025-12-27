330c92952f0318fcb48cc5143d9a6fda873c451a
Roofa <roofa.etagiuri1@gmail.com>
2025-12-27 15:57:08 -0600

    added new files to project

--- client/src/sections/Bio.tsx
+++
@@ -1,7 +0,0 @@
-export default function Bio() {
-  return (
-    <div className="space-y-4 text-neutral-300">
-      <p>Write your bio here...</p>
-    </div>
-  );
-}
--- client/src/sections/Projects.tsx
+++
@@ -1,7 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="space-y-4 text-neutral-300">
-      <p>Write your projects here. Include a few highlights: stacks you use, what you like building, and what you’re looking for.</p>
-    </div>
-  );
-}
